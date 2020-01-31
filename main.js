73ca2b61969f3ebb9632d1d6d539fb800ec609a5
guptashekhhar <shekhar1@getnuo.com>
2020-02-01 01:13:25 +0530

    updated main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var kue = require('kue');
 var jobs = kue.createQueue();
 
+
+// Job queue
 function newJob (name){
     name = name || 'Shekhar Gupta'
     var job = jobs.create('new_job', {
@@ -17,10 +19,59 @@ function newJob (name){
 }
 
 jobs.process('new_job', function (job, done){
-    console.log('Job', job.id, 'is done');
+    // console.log('Job', job.id, 'is done');
     done && done();
 })
 
 setInterval(function (){
     newJob('Send_Email')
 }, 3000);
+
+
+
+//parent child queue
+function parentJob (done){
+    var job = jobs.create('parent', {
+      type: 'PARENT'
+    });
+    job
+      .on('complete', function (){
+        console.log('Job', job.id, 'of type', job.data.type, 'is done');
+        done();
+      })
+      .on('failed', function (){
+        console.log('Job', job.id, 'of type', job.data.type, 'has failed');
+        done();
+      })
+    job.save();
+  }
+  
+  function childJob (done){
+    var job = jobs.create('child', {
+      type: 'CHILD'
+    });
+    job
+      .on('complete', function (){
+        console.log('Job', job.id, 'of type', job.data.type, 'is done');
+        done();
+      })
+      .on('failed', function (){
+        console.log('Job', job.id, 'of type', job.data.type, 'has failed');
+        done();
+      })
+    job.save();
+  }
+  
+  jobs.process('parent', function (job, done){
+    /* carry out all the parent job functions here */
+    childJob(done);
+  })
+  
+  jobs.process('child', function (job, done){
+    /* carry out all the child job functions here */
+    done();
+  })
+  
+  setInterval(function (){
+      parentJob(function () {})
+  },3000);
